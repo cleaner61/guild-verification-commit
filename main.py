139b3c02745d926082fed7f2577c667d5c6725f5
cleaner61 <80271105+cleaner61@users.noreply.github.com>
2026-03-05 17:40:50 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:40:47.060Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T14:40:49.500Z
+Commit: Fix memory leak in event handlers
