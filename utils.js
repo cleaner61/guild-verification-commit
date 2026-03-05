85e6b5de8a28ba5f869c258dd8f584e84b4705e7
cleaner61 <80271105+cleaner61@users.noreply.github.com>
2026-03-05 17:38:02 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:37:31.085Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T14:38:01.576Z
+Commit: Fix broken links in documentation
