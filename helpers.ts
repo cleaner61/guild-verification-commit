ae38a8eec40e457c8bb3d48973fda26f9a890e44
cleaner61 <80271105+cleaner61@users.noreply.github.com>
2026-03-05 17:38:10 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:37:48.736Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T14:38:09.474Z
+Commit: Add dark mode support
