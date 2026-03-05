e90f2d976fbe6c3d509fba99e9ace592a2eebd87
cleaner61 <80271105+cleaner61@users.noreply.github.com>
2026-03-05 17:35:40 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:35:27.246Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T14:35:40.390Z
+Commit: Fix broken links in documentation
