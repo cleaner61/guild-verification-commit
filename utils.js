4020c455b3640b9a0301817014d5ee2d205fc12b
cleaner61 <80271105+cleaner61@users.noreply.github.com>
2026-03-05 17:39:32 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:38:01.576Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T14:39:32.039Z
+Commit: Fix CSS styling bug on mobile devices
