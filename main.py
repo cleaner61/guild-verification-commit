b2476d557586f2b3f636f5360daadc224ad29421
cleaner61 <80271105+cleaner61@users.noreply.github.com>
2026-03-05 17:40:47 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:40:38.445Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T14:40:47.060Z
+Commit: Fix broken links in documentation
