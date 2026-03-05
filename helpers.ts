6b32051f00b342f01c8436707128648beb32fc08
cleaner61 <80271105+cleaner61@users.noreply.github.com>
2026-03-05 17:40:27 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:40:22.580Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T14:40:27.044Z
+Commit: Fix memory leak in event handlers
