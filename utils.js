a2db949c02786a0087d754ea186f2d5a23b54ff4
cleaner61 <80271105+cleaner61@users.noreply.github.com>
2026-03-05 17:39:54 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:39:48.493Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T14:39:53.773Z
+Commit: Resolve timezone issues in date handling
